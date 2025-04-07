a7345133745971b1fee96c90b3bd31545be13a58
Alvaro Aceval <alvaro.aceval.morales16@gmail.com>
2025-04-07 08:44:56 -0600

    Colores del formulario y Hints

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-import 'package:banco_de_proyectos/pages/Dashboard_page.dart';
-import 'package:banco_de_proyectos/pages/form_contacto-empresa.dart';
 import 'package:banco_de_proyectos/pages/form_proyecto.dart';
-import 'package:banco_de_proyectos/pages/login_page.dart';
+//import 'package:banco_de_proyectos/pages/login_page.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
